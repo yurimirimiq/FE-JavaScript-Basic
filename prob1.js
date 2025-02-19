986c9eae44ccc873f61fa158e248979cb7dfe471
unknown <jeonsubin5156@inha.edu>
2025-02-19 15:37:32 +0900

    prob update

---
+++ prob1.js
@@ -0,0 +1,7 @@
+//1. 함수 로직 작성 문제
+
+function solution(num, total) {
+  let answer = [];
+
+  return answer;
+}
---
+++ prob2.js
@@ -0,0 +1,6 @@
+//2. map활용 문제
+const numbers = [1, 2, 3, 4, 5];
+
+let answer;
+
+console.log(answer);
