228635a5ec8b7bd84c4e74707bb42a597b7b26d1
강유림 <yurimirimiq@gmail.com>
2025-04-01 22:17:43 +0900

    유리미

--- prob1.js
+++ prob1.js
@@ -3,12 +3,14 @@
 function solution(num, total) {
   let answer = [] ;
   let aver = total / num;
-  for(let i = 0; i<num I ++)
+  for(let i = 0; i<num;i++)
+  {
+    answer[i]
+  }
 }
-
 
 console.log(solution(3, 12));
 console.log(solution(4, 14));
 console.log(solution(5, 5));
 
-//1. 강유림 commit 
+// 강유림 commit 
