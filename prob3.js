0039fd8e92c38db4fb558ddb2e497b1f8c608736
강유림 <yurimirimiq@gmail.com>
2025-04-02 17:55:10 +0900

    유림3번

--- prob3.js
+++ prob3.js
@@ -6,14 +6,37 @@ let likelion = [
 ];
 
 // 1번 문제 코드작성
+for(let i =0; i<likelion.length.length; i++)
+{
+  for(let j=0;j<likelion.length-1-i;j++)
+  {
+    if(likelion[j].age>likelion[j+1].age){
+      [likelion[j].age,likelion[j+1].age] = [likelion[j+1].age,likelion[j].age]
+    }
 
+  }
+}
 // 2번 문제 코드작성
 let result2 = [];
-
+for(let i=0; i<likelion.length;i++)
+  {
+    if(likelion[i].age>=24)
+    {
+      result2[i]=likelion[i];
+    }
+  }
 console.log(result2);
 
 // 3번 문제 코드작성
 let targetName = "전수빈";
 let result3 = null;
+
+for(let i=0; i<likelion.length;i++)
+  {
+    if(likelion[i].name=="전수빈")
+    {
+      result3=likelion[i].part;
+    }
+  }
 
 console.log(result3);
