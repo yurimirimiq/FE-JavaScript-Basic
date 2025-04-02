3ddf1bb68fa64699ea521f336aec83e500d725c7
강유림 <yurimirimiq@gmail.com>
2025-04-02 17:53:05 +0900

    유림과제

--- prob2.js
+++ prob2.js
@@ -2,5 +2,13 @@
 const numbers = [1, 2, 3, 4, 5];
 
 let answer;
+let answer = numberrs.map(function(element){
+    if (element%==1)
+    {
+        element = "홀수"
+    }
+    else element = "짝수"
+    return element
 
+})
 console.log(answer);
